909bc3af8d9744d4e2aa35fd1e022162732a42d3
Suchandra Thapa <sthapa@ci.uchicago.edu>
2014-05-14 13:00:33 -0500

    Initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from distutils.core import setup
+import glob,re, os
+
+
+
+def get_version():
+  """
+  Gets version from script file
+  """
+  buffer = open('scripts/make_connect_copy').read()
+  match = re.search("VERSION\s+=\s+'(.*)'", buffer)
+  return match.group(1)
+  
+setup(name='connect-copy',
+      version=get_version(),
+      description='Package for connect-copy and associated scripts',
+      author='Suchandra Thapa',
+      author_email='sthapa@ci.uchicago.edu',
+      scripts=['scripts/make_connect_copy'],
+      data_files=[('share/connect-copy', ['scripts/connect_copy.py'])]
+      )
+
